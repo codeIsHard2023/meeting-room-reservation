8f861118fb7827994b64b6fe20af1cb8ad611b48
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2024-04-15 12:24:29 +0200

    post route created to add new reservations; get route created to read existing reservations

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about" v-if="meetingRooms.length > 0">
     <h1>This is an about page </h1>
-    <img :src="$env.VITE_BACKEND_URL + meetingRooms[0].roomPhoto" :alt="meetingRooms[0].name">
-    <p> This is meeting room {{ meetingRooms[0].name }} its capacity is {{ meetingRooms[0].capacity }}</p>
+    <img :src="$env.VITE_BACKEND_URL + meetingRooms[1].roomPhoto" :alt="meetingRooms[1].name">
+    <p> This is meeting room {{ meetingRooms[1].name }} its capacity is {{ meetingRooms[1].capacity }}</p>
     <div class="roomEquipement">
       <img :src="$env.VITE_BACKEND_URL + meetingRooms[0].equipements[0].imgSrc" :alt="meetingRooms[0].equipements[0].name">
       <img :src="$env.VITE_BACKEND_URL + meetingRooms[0].equipements[1].imgSrc" :alt="meetingRooms[0].equipements[1].name">
@@ -16,12 +16,20 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      meetingRooms: []
+      meetingRooms: [],
+      reservations: []
     }
   },
   mounted() { 
-    axios.get('http://localhost:3330/api/test-all-rooms')
+
+    // fetching rooms data
+    axios.get('http://localhost:3330/api/all-rooms')
     .then((res) => this.meetingRooms = res.data.rooms)
+    .catch((err) => console.error("Error fetching data:", err))
+
+    //fetching reservation data
+    axios.get('http://localhost:3330/api/all-reservations')
+    .then((res) => {this.reservations = res.data.reservations; console.log(this.reservations)})
     .catch((err) => console.error("Error fetching data:", err))
   }
 }  
